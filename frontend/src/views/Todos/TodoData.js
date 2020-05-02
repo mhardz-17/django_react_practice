357b1c2ded90285d46511378eb6b9e825e1259b7
Mardy A. dela Cruz <mardy.codev@gmail.com>
2020-05-02 15:23:09 +0800

    Remove initial frontend code

--- frontend/src/views/Todos/TodoData.js
+++
@@ -1,9 +0,0 @@
-const todoData = [
-  {id: 1, title: 'Samppa Nori', description: 'D 1',completed:false,created_at:'2018/02/01', updated_at: '2018/02/01'},
-  {id: 2, title: 'Estavan Lykos', description: 'D 2',completed:false,created_at:'2018/02/01', updated_at: '2018/02/01'},
-  {id: 3, title: 'Chetan Mohamed', description: 'D 3',completed:true,created_at:'2018/02/01', updated_at: '2018/02/01'},
-  {id: 4, title: 'Derick Maximinus', description: 'D 4',completed:false,created_at:'2018/02/01', updated_at: '2018/02/01'},
-  {id: 5, title: 'Friderik Dávid',description: 'D 5',completed:true,created_at:'2018/02/01', updated_at: '2018/02/01'},
-]
-
-export default todoData
